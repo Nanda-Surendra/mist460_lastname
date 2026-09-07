cef1eed7c906dcfad89d0f9e28d7c77422b40955
Nanda Surendra <Nanda.Surendra@mail.wvu.edu>
2026-09-07 23:36:00 +0200

    Created Room and RoomAvailability tables

--- Data/RDB/CreateTables.sql
+++ Data/RDB/CreateTables.sql
@@ -11,3 +11,25 @@ FOR LOGIN NandaSurendra;
 
 ALTER ROLE db_owner ADD MEMBER NandaSurendra;
 */
+
+drop table if exists Room;
+drop table if exists RoomAvailability;
+go
+
+create table Room
+(
+    RoomID int identity(1,1) primary key,
+    RoomNumber varchar(10) not null,
+    Whiteboard bit not null -- for true or false
+);
+
+go
+
+create table RoomAvailability
+(
+    RoomAvailabilityID int identity(1,1) primary key,
+    RoomID int not null,
+    AvailableDate date not null,
+    AvailableStartTime time not null,
+    constraint FK_RoomAvailability_Room foreign key (RoomID) references Room(RoomID)
+);
